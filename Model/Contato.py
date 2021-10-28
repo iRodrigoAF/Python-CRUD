b87573a6ea8cfbc28b2daca8cb5f2b3337b6244b
Rodrigo Angelini Ferrari <86326657+iRodrigoAF@users.noreply.github.com>
2021-10-27 22:45:53 -0300

    Delete Contato.py

--- Model/Contato.py
+++
@@ -1,6 +0,0 @@
-from Model.Pessoa import Pessoa
-class Contato(Pessoa):
-
-    def __init__(self, nome, endereco, bairro, cep, cidade, estado, id_empresa):
-        self.nome = nome
-        self.endereco = endereco
